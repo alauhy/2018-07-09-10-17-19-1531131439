a29779308c42d09e5c76a600cefc1ce0fa860fc9
lauhiuyee <lauhiuyee@lauhiuyeede-MacBook-Pro.local>
2019-07-12 01:30:50 +0800

    finished pratice11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,72 @@
 package practice11;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> students = new ArrayList<Student>();
+    private List<Teacher> observers = new ArrayList<Teacher>();
+
+
+    public void attach(Teacher observer){
+        observers.add(observer);
+    }
+
+    public void notifyAllObservers(Student student,int mode){
+
+        for (Teacher observer : observers) {
+            if(mode==1){
+                observer.update(student,this.getDisplayName());
+            }
+            if(mode==2){
+                observer.update2(student,this.getDisplayName());
+            }
+        }
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+
+    }
+    public String getDisplayName(){
+        return "Class "+number;
+    }
+    public void appendMember(Student student) {
+
+        students.add(student);
+        notifyAllObservers(student,1);
+
+    }
+    public void assignLeader(Student student){
+        if(students!=null && !students.contains(student)){
+            System.out.println("It is not one of us.");
+        }
+        else {
+            this.leader=student;
+            notifyAllObservers(student,2);
+        }
+
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public boolean isIn(Student student){
+        if(students!=null && students.contains(student)){
+            return true;
+        }
+        else return false;
+    }
+
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,66 @@
 package practice11;
 
-public class Teacher {
+import java.util.List;
+
+public class Teacher extends Person{
+    private List<Klass> classes;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+        for(Klass klass : classes){
+            klass.attach(this);
+        }
+    }
+
+
+
+    public void update(Student student,String classNumer) {
+        String str= "I am "+super.getName()+". I know "+student.getName()+" has joined "+classNumer+".";
+        System.out.println(str);
+    }
+    public void update2(Student student,String classNumer) {
+        String str= "I am "+super.getName()+". I know "+student.getName()+" become Leader of "+classNumer+".";
+        System.out.println(str);
+    }
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public String introduce() {
+        String str=super.introduce()+" I am a Teacher. I teach Class ";
+        if(classes!=null) {
+            for (Klass klass : classes) {
+                str += klass.getNumber() + ", ";
+
+            }
+            str=str.substring(0,str.length()-2);
+            str+=".";
+        }
+        else str="My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.";
+        return str;
+
+    }
+    public String introduceWith(Student student){
+        if(classes.contains(student.getKlass())){
+            return  super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+        }
+        else return super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+    }
+    public boolean isTeaching(Student student){
+        boolean flag = false;
+        for(Klass klass:classes){
+            if(klass.isIn(student) || klass==student.getKlass()){
+                flag = true;
+            }
+        }
+        return flag;
+
+    }
 }
