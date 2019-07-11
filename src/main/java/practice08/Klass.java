96976bdd45b827565ca3145f7f9f678c7e45b32c
lauhiuyee <lauhiuyee@lauhiuyeede-MacBook-Pro.local>
2019-07-11 23:00:53 +0800

    finished pratice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,24 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+
+    }
+    public String getDisplayName(){
+        return "Class "+number;
+    }
+    public void assignLeader(Student student){
+        this.leader=student;
+    }
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,46 @@
 package practice08;
 
+import java.util.Objects;
+
 public class Person {
+    private final String name;
+    private final int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, age, id);
+    }
+
+    private final int id;
+
+    public Person(int id,String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is "+this.name+". I am "+this.age+" years old.";
+    }
+
+    public String basicIntroduce() {
+        return "My name is "+this.name+". I am "+this.age+" years old.";
+    }
 }
